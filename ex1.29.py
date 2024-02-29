5f989f3566eed76f2258d8328a9fde18256ff305
isabellaa-cg <146957409+isabellaa-cg@users.noreply.github.com>
2024-02-29 10:12:15 -0300

    Add files via upload

---
+++ ex1.29.py
@@ -0,0 +1,6 @@
+nomes = {"nome": "Isabella", "nome2": "Rebecca", "nome3": "Fabricia", "nome4": "Gilbert", "nome5": "Rafaela"}
+cor_do_olho = {"olho": "castanho", "olho2": "castanho", "olho3": "castanho", "olho4": "castanho", "olho5": "castanho",}
+idade = {"idade": 18, "idade2": 17, "idade3": 48, "idade4": 48, "idade5": 18,}
+print(nomes)
+print(cor_do_olho)
+print(idade)
---
+++ ex2.8.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite um número: "))
+string = str(numero)
+
+if string.endswith('.0'):
+    print("É um número real!")
+else:
+    print("Não é um número real!")
